f3e13a9827383fe701e99909493e82f06e5e7173
ntgiang <giangk9a1@gmail.com>
2026-04-21 14:07:13 +0700

    docs(swagger): document current user endpoints

--- src/common/swagger/user.swagger.js
+++ src/common/swagger/user.swagger.js
@@ -6,7 +6,49 @@ export const user = {
             responses: {
                 200: {
                     description: "Lấy thông tin người dùng thành công",
-                }
+                },
+            },
+        },
+    },
+    "/user/me/created-images": {
+        get: {
+            tags: ["User"],
+            summary: "Lấy danh sách ảnh do user hiện tại tạo",
+            parameters: [
+                {
+                    name: "page",
+                    in: "query",
+                    schema: { type: "integer", default: 1, minimum: 1 },
+                },
+                {
+                    name: "pageSize",
+                    in: "query",
+                    schema: { type: "integer", default: 10, minimum: 1 },
+                },
+            ],
+            responses: {
+                200: { description: "Lấy danh sách ảnh đã tạo thành công" },
+            },
+        },
+    },
+    "/user/me/saved-images": {
+        get: {
+            tags: ["User"],
+            summary: "Lấy danh sách ảnh user hiện tại đã lưu",
+            parameters: [
+                {
+                    name: "page",
+                    in: "query",
+                    schema: { type: "integer", default: 1, minimum: 1 },
+                },
+                {
+                    name: "pageSize",
+                    in: "query",
+                    schema: { type: "integer", default: 10, minimum: 1 },
+                },
+            ],
+            responses: {
+                200: { description: "Lấy danh sách ảnh đã lưu thành công" },
             },
         },
     },
